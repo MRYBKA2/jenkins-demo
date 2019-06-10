4b21fbe63e5f0ec043c6935335ae4a9d4ef31882
MRYBKA2 <42096959+MRYBKA2@users.noreply.github.com>
2019-06-11 00:13:44 +0300

    Update MarkWaite.java

--- src/com/markwaite/MarkWaite.java
+++ src/com/markwaite/MarkWaite.java
@@ -2,7 +2,7 @@ package com.markwaite;
 
 public class MarkWaite {
     public final String getName() {
-        return "Mark WaiteSSS";
+        return "Mark E Waite";
     }
 
     public final String getBlogURL() {
